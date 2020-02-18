1a7123a8cfe96d81448f230c59ac87e5d062d873
angelikajeziorska <angjeziorska@wp.pl>
2020-02-18 20:07:52 +0100

    Fully formatted workout types

--- WODder/WODder/NewWorkout.swift
+++ WODder/WODder/NewWorkout.swift
@@ -103,32 +103,26 @@ class NewWorkout: FormViewController {
                 $0.tag = "emom_t"
                 $0.hidden = "$segments != 'EMOM'"
             }
-            <<< IntRow() {
+            <<< PickerInputRow<String>(){
                 $0.title = "Every: "
-                $0.placeholder = "' for minutes '' for seconds'"
-                $0.add(rule: RuleGreaterThan(min: 0))
-                $0.add(rule: RuleSmallerThan(max: 1000))
-                }
-                .cellUpdate { cell, row in
-                    if !row.isValid {
-                        cell.titleLabel?.textColor = .red
+                $0.options = []
+
+                var minutes = 0, seconds = 15
+                 while minutes <= 10 {
+                    if seconds == 0 {
+                        $0.options.append("\(minutes):\(seconds)0")
                     }
-                }
-                .onRowValidationChanged { cell, row in
-                    let rowIndex = row.indexPath!.row
-                    while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
-                        row.section?.remove(at: rowIndex + 1)
+                    else {
+                        $0.options.append("\(minutes):\(seconds)")
                     }
-                    if !row.isValid {
-                        for (index, validationMsg) in row.validationErrors.map({ $0.msg }).enumerated() {
-                            let labelRow = LabelRow() {
-                                $0.title = validationMsg
-                                $0.cell.height = { 30 }
-                            }
-                            let indexPath = row.indexPath!.row + index + 1
-                            row.section?.insert(labelRow, at: indexPath)
-                        }
+                    seconds = seconds + 15
+                    if seconds == 60 {
+                        minutes += 1
+                        seconds = 0
                     }
+                 }
+                $0.options.append("\(minutes):\(seconds)0")
+                $0.value = $0.options.first
             }
 
             <<< IntRow() {
@@ -162,24 +156,107 @@ class NewWorkout: FormViewController {
                 $0.tag = "amrap_t"
                 $0.hidden = "$segments != 'AMRAP'"
             }
-            <<< TextRow(){
-                $0.title = "Which is your favourite actor?"
-            }
-
-            <<< TextRow(){
-                $0.title = "Which is your favourite film?"
+            <<< IntRow() {
+                $0.title = "Time cap: "
+                $0.placeholder = "in minutes"
+                $0.add(rule: RuleGreaterThan(min: 0))
+                $0.add(rule: RuleSmallerThan(max: 1000))
+                }
+                .cellUpdate { cell, row in
+                    if !row.isValid {
+                        cell.titleLabel?.textColor = .red
+                    }
+                }
+                .onRowValidationChanged { cell, row in
+                    let rowIndex = row.indexPath!.row
+                    while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
+                        row.section?.remove(at: rowIndex + 1)
+                    }
+                    if !row.isValid {
+                        for (index, validationMsg) in row.validationErrors.map({ $0.msg }).enumerated() {
+                            let labelRow = LabelRow() {
+                                $0.title = validationMsg
+                                $0.cell.height = { 30 }
+                            }
+                            let indexPath = row.indexPath!.row + index + 1
+                            row.section?.insert(labelRow, at: indexPath)
+                        }
+                    }
             }
         
         +++ Section(){
             $0.tag = "tabata_t"
             $0.hidden = "$segments != 'TABATA'"
         }
-        <<< TextRow(){
-            $0.title = "Which is your favourite actor?"
+        <<< IntRow() {
+            $0.title = "Rounds:"
+            $0.add(rule: RuleGreaterThan(min: 0))
+            $0.add(rule: RuleSmallerThan(max: 1000))
+            }
+            .cellUpdate { cell, row in
+                if !row.isValid {
+                    cell.titleLabel?.textColor = .red
+                }
+            }
+            .onRowValidationChanged { cell, row in
+                let rowIndex = row.indexPath!.row
+                while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
+                    row.section?.remove(at: rowIndex + 1)
+                }
+                if !row.isValid {
+                    for (index, validationMsg) in row.validationErrors.map({ $0.msg }).enumerated() {
+                        let labelRow = LabelRow() {
+                            $0.title = validationMsg
+                            $0.cell.height = { 30 }
+                        }
+                        let indexPath = row.indexPath!.row + index + 1
+                        row.section?.insert(labelRow, at: indexPath)
+                    }
+                }
         }
 
-        <<< TextRow(){
-            $0.title = "Which is your favourite film?"
+        <<< PickerInputRow<String>(){
+            $0.title = "Work: "
+            $0.options = []
+
+            var minutes = 0, seconds = 15
+             while minutes <= 14 {
+                if seconds == 0 {
+                    $0.options.append("\(minutes):\(seconds)0")
+                }
+                else {
+                    $0.options.append("\(minutes):\(seconds)")
+                }
+                seconds = seconds + 15
+                if seconds == 60 {
+                    minutes += 1
+                    seconds = 0
+                }
+             }
+            $0.options.append("\(minutes):\(seconds)0")
+            $0.value = $0.options.first
+        }
+        
+        <<< PickerInputRow<String>(){
+            $0.title = "Rest: "
+            $0.options = []
+
+            var minutes = 0, seconds = 15
+             while minutes <= 14 {
+                if seconds == 0 {
+                    $0.options.append("\(minutes):\(seconds)0")
+                }
+                else {
+                    $0.options.append("\(minutes):\(seconds)")
+                }
+                seconds = seconds + 15
+                if seconds == 60 {
+                    minutes += 1
+                    seconds = 0
+                }
+             }
+            $0.options.append("\(minutes):\(seconds)0")
+            $0.value = $0.options.first
         }
         
 //        add exercises
